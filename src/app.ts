f10c24a148fcd2e7777eae7828d11b14891ac944
Murilo Oliveira <murilo.oliveira@dbseller.com.br>
2024-08-12 19:11:26 -0300

    ajuste rota

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 import "express-async-errors"
 import express from "express";
+import cors from "cors";
 import router from "./routes";
-import path from "path";
 import "./providers/brokers/kafka/consumers"
 import { errorMiddleware } from "./middlewares/errorMiddleware";
 
 const app = express();
+app.use(cors());
 app.use(router);
 app.use(express.json());
 app.use(express.static("uploads"));
